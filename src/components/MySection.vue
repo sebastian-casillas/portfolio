327ccb0ffb61616a83138bb426a3ecb4e9c35ebe
Sebastian Casillas <sebastian.casillas@outlook.com>
2021-10-26 08:28:38 +0000

    Lot of changes to the format

--- src/components/MySection.vue
+++ src/components/MySection.vue
@@ -1,94 +1,101 @@
 <template>
-    <div no-gutters style="padding:.6mm 0 .5mm 0;">
-        <div class="grow" style="padding:0 1mm 0 1mm">
-            <h5 v-html="experience.title"></h5>
-            <h6 v-html="experience.subtitle"></h6>
-            <template v-for="item of experience.items" :key="item">
-                <ul>
-                    <li v-html="item"></li>
-                </ul>
-
-            </template>
-
+    <div class="section">
+        <div class="date_block">
+            <span class="date_text">
+                {{experience.date_start}}<br>{{experience.date_end}}
+            </span>
         </div>
-        <div min-width="8mm" class="shrink px-0">
-            <div width="8mm"  height="100%">
-                <div class="vertical-text text-no-wrap"
-                     :style="'transform: rotate(270deg) translateX('+ x_spacing +'%) translateY(-315%)'">
-                    {{experience.date_start}}<br>{{experience.date_end}}
-                </div>
+        <div class="info_block">
+            <div class="info_block_detail"/>
+            <div class="info_block_content">
+                <p v-if="experience.company">{{company}}</p>
+                <h5 v-html="experience.title"></h5>
+                <h6 v-html="experience.subtitle"></h6>
+                
+                <ul>
+                    <li v-for="item of experience.items" 
+                        :key="item"
+                        v-html="item"
+                        />
+                </ul>
             </div>
 
         </div>
     </div>
+
+
 </template>
 
 <script>
     export default {
         name: "MySection",
-        props: ['experience', 'x_spacing'],
-        computed: {
-            // a computed getter
-            x_calculated: function () {
-                return this.x_spacing - 40;
-            }
-        }
+        props: ['experience'],
     }
 </script>
 
-<style scoped>
-    h1{
-        font-size: 9mm;
-        padding-bottom: .3mm;
-    }
+<style lang="scss">
 
-    h2{
-        font-size: 6mm;
-        padding-bottom: .3mm;
-    }
+    .section{
+        max-width: 100%;
+        display: flex;
+        margin-bottom: 3mm;
 
-    h3{
-        font-size: 4.5mm;
-        padding-bottom: 1mm;
-    }
+        .date_block{
+            position: relative;
+            padding-top: 1mm;
+            margin-right: 1mm;
 
-    h4{
-        font-size: 3mm;
-        padding-bottom: 1mm;
-    }
+            width: 6mm;
+            max-width: 6mm;
+            min-width: 6mm;
 
-    h5{
-        font-size: 4mm !important;
-        padding-bottom: .5mm !important;
-        height:5.4mm;
-    }
+            display:flex;
+            justify-content: flex-end;
+            
 
-    h6{
-        font-size: 3.2mm !important;
-        padding-bottom: .5mm !important;
-        line-height:4.6mm;
-        color: #666666;
-    }
+            & > .date_text{
+                font-size: 2mm;
+                font-weight: 600;
+                color: #666;
+                line-height: 2.8mm;
+                writing-mode: vertical-rl;
+                letter-spacing: .05mm;
 
-    ul{
-        padding-left: 4mm;
-    }
-
-    li{
-        font-size: 2.8mm;
-        font-weight: 400;
-        letter-spacing: 0;
-        margin-bottom: .2mm;
-    }
+                text-align: right;
+                transform: rotate(180deg)
+            }
+          }
 
 
-    .vertical-text {
-        font-size: 2.4mm;
-        font-weight: 600;
-        color: #555555;
-        height: 5.5mm;
-        width: 40mm;
-        /* text-wrap: none; */
+        .info_block{
+            display:inline-block;
+            border-left: solid .3mm #888;
+
+            .info_block_detail{
+                height: .2mm; 
+                width: 10mm; 
+                border-top: solid .3mm #888;
+            }
+
+            .info_block_content{
+                padding-left: 1.5mm;
+                padding-top: .8mm;
+            }
+
+            ul{
+                margin: 0 0 0 2mm;
+                padding-left: 3mm;
+            }
+
+            li{
+                color: #333;
+                font-size: 2.8mm;
+                font-weight: 400;
+                letter-spacing: 0;
+                margin-bottom: .2mm;
+            }
+
+        }
     }
 
 </style>
