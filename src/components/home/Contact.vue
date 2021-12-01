a7eaab73edcdcafc031f3e695d4914f9ab256aaf
Sebastian Casillas <sebastian.casillas@outlook.com>
2021-12-01 16:02:17 +0000

    Fixing contact card

--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -2,16 +2,18 @@
   <div id="contact_component" class="full-width row justify-center items-center content-center">
 
     <div class="presentation_background" :style="max_width">
-      <q-responsive  :ratio="2.2"  >
-
-        <div class="row pressentation_card">
 
 
-          <q-avatar size="250px" class="q-mx-xl col-grow">
-            <img src="/assets/profile_circle.png">
-          </q-avatar>
+        <div class="row pressentation_card q-pa-xl">
 
-          <div class="q-mx-md col-grow">
+          <div class="col-md-5 col-sm-12 col-xs-12 row justify-center align-center">
+            <q-avatar size="250px" >
+              <img src="/assets/profile_circle.png">
+            </q-avatar>
+          </div>
+
+
+          <div class="q-pl-md col-md-7 col-sm-12 col-xs-12 row align-center">
             <h2 class="q-ma-sm">Sebastian</h2>
             <h3 class="q-ma-sm">Casillas Perez</h3>
 
@@ -24,7 +26,6 @@
 
         </div>
 
-      </q-responsive>
     </div>
 
 
@@ -77,18 +78,14 @@ export default {
 
     .pressentation_card{
 
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
       background-color: white; 
       height: 100%; 
       width: 100%;
       color: black;
 
-box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
--webkit-box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
--moz-box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
+      box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
+      -webkit-box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
+      -moz-box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
     }
 
     .contact_buttons{
@@ -98,13 +95,6 @@ box-shadow: 16px 24px 50px -11px rgba(0,0,0,0.76);
         margin-bottom: 10px;
       }
 
-    }
-
-    .background_radial{
-      height: 100%;
-      position:relative;
-      background-color: red;
-      background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);
     }
 
   }
