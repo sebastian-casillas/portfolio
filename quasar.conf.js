cdf392e6e9ef297e09ba25a142cae709c46d4c56
Sebastian Casillas <bastian.kass@gmail.com>
2023-03-07 18:36:48 +0000

    New background

--- quasar.conf.js
+++ quasar.conf.js
@@ -36,7 +36,8 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.scss'
+      'app.scss',
+      'background.scss'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
