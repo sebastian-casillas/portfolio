4aefd4a868dd628d82b22b437d6b71a20c4278b7
Sebastian Casillas <bastian.kass@gmail.com>
2023-03-08 08:43:00 +0000

    Making background move faster

--- src/pages/Workflow.vue
+++ src/pages/Workflow.vue
@@ -20,18 +20,6 @@
             <p style="line-height: 1rem;">{{c.content}}</p>
         </div>
 
-
-        <div class="col-md-6 col-sm-12 col-xs-12 q-px-xl q-my-sm competence_card">
-            <h5 class="q-my-md" style="line-height: 2rem;">Keep updated and relevant, always!</h5>
-            <div style="height: 80px; width: 100%; padding: 10px; background-color: #333;">
-                <p style="line-height: 1rem; color: #CCC; font-family: consolas; font-weight: 600;">
-
-                    <span style="color: #4D4;">sebastian.casillas@mycomputer:</span> /usr/projects/onlineportfolio# git pull
-                </p>
-            </div>
-            
-        </div>
-
     </div>
 
 
